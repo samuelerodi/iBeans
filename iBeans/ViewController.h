781c7fd5eaf5637c365fa13ce58fa325a0b34d04
Sam <samuele.rodi@gmail.com>
2014-11-24 00:47:33 +0100

    Initial Commit

---
+++ iBeans/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iBeans
+//
+//  Created by Sam on 11/24/14.
+//  Copyright (c) 2014 Sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
